134156da3e053026a78869f5532e4bd27f0c65ee
kazizi-dev <kouroshazizi2012@gmail.com>
2021-05-04 02:39:08 -0700

    refactor etl method, and implement getting training data

--- train_chatbot.py
+++ train_chatbot.py
@@ -17,13 +17,10 @@ from keras.layers import Dense, Activation, Dropout
 from keras.optimizers import SGD
 import random
 
-def perform_ETL():
-    data_file = open('intents.json', encoding='utf-8').read()
-    intents = json.loads(data_file)
-
-    words=[]
-    tags = []
-    documents = []
+def perform_ETL(intents):
+    words=[]            # list of words
+    tags = []           # list of tags
+    documents = []      # an array of tuples
     ignore_words = ['?', '!']
 
     # extract data from intents
@@ -41,7 +38,7 @@ def perform_ETL():
     temp = []
     for w in words:
         if w not in ignore_words:
-            temp.append(lemmatizer.lemmatize(w.lower()))
+            temp.append(lemmatizer.lemmatize(w.lower()))    # create the base words
 
     words = sorted(list(set(temp)))
     tags = sorted(list(set(tags)))
@@ -49,13 +46,51 @@ def perform_ETL():
     return documents, tags, words
 
 
-if __name__ == "__main__":
-    documents, tags, words = perform_ETL()
+def get_training_data(documents, tags, words):
+    training_data = []       # training data
+    list_of_zeros = [0 for _ in range(len(tags))]    # an array of empty 0's
 
+    for doc in documents:
+        bag = []                # a bag of 0 or 1 representing boolean
+        pattern_words = doc[0]  # get the list of pattern words
+
+        temp = []
+        for word in pattern_words:
+            temp.append(lemmatizer.lemmatize(word.lower()))     # create the base words
+        pattern_words = temp
+
+        for word in words:
+            if word in pattern_words:
+                bag.append(1)       # the words with matching patterns
+            else:
+                bag.append(0)
+    
+        output_row = list(list_of_zeros)
+        output_row[tags.index(doc[1])] = 1
+        training_data.append([bag, output_row])
+
+    random.shuffle(training_data)
+    return training_data
+
+
+if __name__ == "__main__":
+    data_file = open('./data/intents.json', encoding='utf-8').read()
+    intents = json.loads(data_file)
+
+    documents, tags, words = perform_ETL(intents)
+
+    # print the information after ETL
     print (len(documents), "documents")
     print (len(tags), "classe(s)", tags)
-    print (len(words), "unique lemmatized words", words)
+    print (len(words), "unique lemmatized words")
 
     # save the data as pickle files
-    pickle.dump(words,open('./results/words.pkl','wb'))
-    pickle.dump(tags,open('./results/classes.pkl','wb'))
+    # pickle.dump(words,open('./results/words.pkl','wb'))
+    # pickle.dump(tags,open('./results/classes.pkl','wb'))
+
+    # get x and y training data for the model
+    x_train = []
+    y_train = []
+    for train in get_training_data(documents, tags, words):
+        x_train.append(train[0])
+        y_train.append(train[1])
