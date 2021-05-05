16f43eed20df99b6443cf2b93fd2aef7057f162e
kazizi-dev <kouroshazizi2012@gmail.com>
2021-05-04 23:50:39 -0700

    create NN model

--- train_chatbot.py
+++ train_chatbot.py
@@ -18,6 +18,12 @@ from keras.optimizers import SGD
 import random
 
 def perform_ETL(intents):
+    """
+    Extract the data from intents, and then 
+    transform by performing lemmatisation on
+    the words in the data.
+    """
+
     words=[]            # list of words
     tags = []           # list of tags
     documents = []      # an array of tuples
@@ -47,6 +53,11 @@ def perform_ETL(intents):
 
 
 def get_training_data(documents, tags, words):
+    """
+    Build and return the training data from 
+    document data.
+    """
+
     training_data = []       # training data
     list_of_zeros = [0 for _ in range(len(tags))]    # an array of empty 0's
 
@@ -73,6 +84,33 @@ def get_training_data(documents, tags, words):
     return training_data
 
 
+def create_neural_network_model(x_train, y_train):
+    """
+    Creates a 3 layer neural network model using the
+    sequential model in keras. The model is trained 
+    with stochastic gradient descent.
+    """ 
+
+    # create a model with 3 layers, 128, 64, and # of intents
+    model = Sequential()
+    model.add(Dense(128, input_shape=(len(x_train[0]), ), activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(len(y_train[0]), activation='softmax'))
+    
+    # train model with with stochastic gradient descent and then compile it
+    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+    # fitting and saving the model
+    model.save('./results/chatbot_model.h5', model.fit(np.array(x_train), 
+                                            np.array(y_train), 
+                                            epochs=200, 
+                                            batch_size=5, 
+                                            verbose=1))
+
+
 if __name__ == "__main__":
     data_file = open('./data/intents.json', encoding='utf-8').read()
     intents = json.loads(data_file)
@@ -85,8 +123,8 @@ if __name__ == "__main__":
     print (len(words), "unique lemmatized words")
 
     # save the data as pickle files
-    # pickle.dump(words,open('./results/words.pkl','wb'))
-    # pickle.dump(tags,open('./results/classes.pkl','wb'))
+    pickle.dump(words,open('./results/words.pkl','wb'))
+    pickle.dump(tags,open('./results/classes.pkl','wb'))
 
     # get x and y training data for the model
     x_train = []
@@ -94,3 +132,5 @@ if __name__ == "__main__":
     for train in get_training_data(documents, tags, words):
         x_train.append(train[0])
         y_train.append(train[1])
+
+    create_neural_network_model(x_train, y_train)
